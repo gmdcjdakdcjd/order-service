d8ede6ceb32dff7a1dd163be93430988402bfe9e
gmdcjdakdcjd <ysh010223@naver.com>
2024-09-05 11:58:53 +0900

    v12

--- src/main/resources/db/migration/V2__Add_user_audit.sql
+++ src/main/resources/db/migration/V2__Add_user_audit.sql
@@ -1,11 +1,11 @@
+ALTER TABLE orders
+    ADD COLUMN created_by varchar(255);
+
+ALTER TABLE orders
+    ADD COLUMN last_modified_by varchar(255);
+
 -- ALTER TABLE orders
---     ADD COLUMN created_by varchar(255);
+--     ADD COLUMN IF NOT EXISTS created_by varchar(255);
 --
 -- ALTER TABLE orders
---     ADD COLUMN last_modified_by varchar(255);
-
-ALTER TABLE orders
-    ADD COLUMN IF NOT EXISTS created_by varchar(255);
-
-ALTER TABLE orders
-    ADD COLUMN IF NOT EXISTS last_modified_by varchar(255);
+--     ADD COLUMN IF NOT EXISTS last_modified_by varchar(255);
