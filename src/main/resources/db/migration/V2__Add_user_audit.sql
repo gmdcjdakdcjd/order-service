5685be5535ba29637fc6eb0578e8a29277f9f731
gmdcjdakdcjd <ysh010223@naver.com>
2024-09-05 10:54:01 +0900

    v10: add_user_audit change

--- src/main/resources/db/migration/V2__Add_user_audit.sql
+++ src/main/resources/db/migration/V2__Add_user_audit.sql
@@ -1,5 +1,11 @@
-ALTER TABLE orders
-    ADD COLUMN created_by varchar(255);
+-- ALTER TABLE orders
+--     ADD COLUMN created_by varchar(255);
+--
+-- ALTER TABLE orders
+--     ADD COLUMN last_modified_by varchar(255);
 
-ALTER TABLE orders
-    ADD COLUMN last_modified_by varchar(255);
+ALTER TABLE book
+    ADD COLUMN IF NOT EXISTS created_by varchar(255);
+
+ALTER TABLE book
+    ADD COLUMN IF NOT EXISTS last_modified_by varchar(255);
